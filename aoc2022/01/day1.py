4433d836d6a820a81684ef259d7ca8a6c5441437
Matteo Gianello <giane.teo@gmail.com>
2022-12-02 09:20:13 +0100

    Refactory day 01

--- aoc2022/01/day1.py
+++ aoc2022/01/day1.py
@@ -1,26 +1,22 @@
 import os
+import numpy as np
 
-path = os.getcwd()
-with open(f'{path}/aoc2022/01/day01.txt') as f:
+location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+with open(f'{location}/day01.txt') as f:
     lines = f.readlines()
-
 
 calories4Elf = [] 
 sum = 0
 
 for line in lines:
-    if '\n' == line:
+    if line.strip():
+        sum += int(line)
+    else:
         calories4Elf.append(sum)
         sum = 0
-    else:
-        sum = sum + int(line)
-
-
-if len(calories4Elf) == 0: calories4Elf.append(sum);
 
 calories4Elf.sort(reverse=True)
 
-
 print(f'Solution 1 {calories4Elf[0]}')
-print(f'Solution 1 {calories4Elf[0]+calories4Elf[1]+calories4Elf[2]}')
+print(f'Solution 1 {np.sum(calories4Elf[:3])}')
 
