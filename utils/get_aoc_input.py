0600bb23c69ee84a6956f47a3750a245f3aa2f96
Matteo Gianello <giane.teo@gmail.com>
2022-12-02 08:57:27 +0100

    Refactor get_aoc_input file

--- utils/get_aoc_input.py
+++ utils/get_aoc_input.py
@@ -1,25 +1,31 @@
 import requests
 import os
 import sys
+from pathlib import Path
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 4:
     print('Wrong number of arguments')
     exit(0)
 
-day, cookie = int(sys.argv[1]),sys.argv[2]
-headers = {'session': cookie}
-url = f'https://adventofcode.com/2022/day/{day}/input'
+day, year, cookie = int(sys.argv[1]),int(sys.argv[2]), sys.argv[3]
 
+location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+path = Path(location)
+print (f'Working path: {path.parent.absolute()}')
+print(f'Creating path for day {day:02} and year {year} ')
+os.makedirs(f'{path.parent.absolute()}/aoc{year}/{day:02}')
+print('Path created')
+
+print('Retrieving file...')
+headers = {'session': cookie}
+url = f'https://adventofcode.com/{year}/day/{day}/input'
 session = requests.Session()
 resp = session.get(url,cookies=headers)
 
-path = os.getcwd()
-
-print ("current directory is %s" % path)
-
-os.makedirs(f'{path}/aoc2022/{day:02}',)
-
-
-in_file = open(f'{path}/aoc2022/{day:02}/day{day:02}.txt','w')
+filePath = f'{path.parent.absolute()}/aoc2022/{day:02}/day{day:02}.txt'
+print(f'File saved in {filePath}')
+in_file = open(filePath,'w')
 in_file.write(resp.text)
 in_file.close()
+
+print('File correctly saved')
