ff2eed9f3614c616760580f582277ae69339e1a1
Matteo Gianello <giane.teo@gmail.com>
2022-12-07 08:43:47 +0100

    Day06: part 2

--- aoc2022/06/day06.py
+++ aoc2022/06/day06.py
@@ -5,10 +5,27 @@ location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))
 with open(f'{location}/day06.txt') as f:
     lines = f.readlines()
 
+
+"""
+Soluzione parte 1
+"""
 for line in lines:
     line = line.strip()
     for i in range (4, len(line)):
         window = line[i-4: i]
+        print(f'Window: {window} unique: {len(set(window))} size: {len(window)}')
+        if len(set(window))== len(window):
+            print(i)
+            break
+
+
+"""
+Soluzione parte 2
+"""
+for line in lines:
+    line = line.strip()
+    for i in range (14, len(line)):
+        window = line[i-14: i]
         print(f'Window: {window} unique: {len(set(window))} size: {len(window)}')
         if len(set(window))== len(window):
             print(i)
